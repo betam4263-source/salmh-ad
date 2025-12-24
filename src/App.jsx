c363fcfc9192c5b51a4eb6602de8dc19853e2293
betam4263-source <betam4263@gmail.com>
2025-12-24 17:15:38 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Main from "./Main_Page";
 import Login from "./Login";
 
 // export const serverRoute = 'http://localhost:8080'
-export const serverRoute = "https://ssal-se-tt8w.onrender.com";
+export const serverRoute = "https://ssal-se-zdot.onrender.com";
 export const token = localStorage.getItem("token");
 function App() {
   return (
